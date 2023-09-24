ab5aca051309e7c49fde8309679e4d1f277ee977
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-09-24 19:55:14 -0400

    completes video show page partially

--- src/Components/home/VideoListing.jsx
+++ src/Components/home/VideoListing.jsx
@@ -1,14 +1,16 @@
-import {Link} from 'react-router-dom'
-import "./VideoListing.css";
+import { Link } from 'react-router-dom';
+import './VideoListing.css';
 
 export default function VideoListing({ video }) {
+    const videoId = video.id.toString(); // Convert video.id to a string
+
     return (
         <article className="video">
-            <Link to="/videoShow">
-                <img src={video.snippet.thumbnails.default.url} alt="thumbnail" />
-                <h3 className="title">{video.snippet.title}</h3>
+            <Link to={`/videoShow/${videoId}`}>
+                <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />
             </Link>
+                <h3 className="title">{video.snippet.title}</h3>
             <p className="description">{video.snippet.title}</p>
         </article>
     );
-  }
+}
